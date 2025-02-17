dcb7f35f05c56ba953880f721b41791811a7bfa7
cackichi <21010022@study.bsuir.by>
2025-02-17 15:51:32 +0300

    setup + config

--- passenger-service/src/main/java/org/example/PassengerServiceApplication.java
+++ passenger-service/src/main/java/org/example/PassengerServiceApplication.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
 public class PassengerServiceApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(PassengerServiceApplication.class, args);
     }
 }
