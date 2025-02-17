e9383ae935d64de7726330d20d7239e7ba6c8e07
cackichi <21010022@study.bsuir.by>
2025-02-17 16:21:35 +0300

    entity + dto + liqubase + some fix

---
+++ passenger-service/src/main/java/org/example/dto/PassengerDTO.java
@@ -0,0 +1,17 @@
+package org.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class PassengerDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+}
---
+++ passenger-service/src/main/java/org/example/entities/Passenger.java
@@ -0,0 +1,21 @@
+package org.example.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Passenger {
+    @Id
+    private Long id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+}
