786813a9e2e208b1b71e56fc6d596dfdf0e1e155
cackichi <21010022@study.bsuir.by>
2025-02-19 19:21:13 +0300

    entity, enum, dto, modelmappercfg

---
+++ rides-service/src/main/java/org/example/entities/Ride.java
@@ -0,0 +1,21 @@
+package org.example.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Ride {
+    @Id
+    private Long passengerId;
+    private Long driverId;
+    private String pointA;
+    private String pointB;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+}
---
+++ rides-service/src/main/java/org/example/entities/Status.java
@@ -0,0 +1,7 @@
+package org.example.entities;
+
+public enum Status {
+    WAITING,
+    TRAVELING,
+    COMPLETE
+}
